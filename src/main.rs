5d81f63836141ec56ec2f7ee58be75494d925d49
Bogdan Ivanchenko <ivanchenko.b@gmail.com>
2020-08-29 14:09:40 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+mod config;
+mod models;
+
+use crate::models::Status;
+use actix_web::{web, App, HttpServer, Responder};
+use dotenv::dotenv;
+use std::io;
+
+async fn status() -> impl Responder {
+    web::HttpResponse::Ok().json(Status {
+        status: "Ok".to_string(),
+    })
+}
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+    let config = crate::config::Config::from_env().unwrap();
+
+    println!(
+        "Starting server on http://{}:{}/",
+        config.server.host, config.server.port
+    );
+
+    HttpServer::new(|| App::new().route("/", web::get().to(status)))
+        .bind(format!("{}:{}", config.server.host, config.server.port))?
+        .run()
+        .await
+}
